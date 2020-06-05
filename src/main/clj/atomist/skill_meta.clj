fa44fd0ce20eba240588bbe90544ee09948e10e2
Jim Clark <slimslenderslacks@gmail.com>
2020-06-05 13:28:11 -0700

    Bump gcf-java11-clojure to 0.1.3 and spit metadata

--- src/main/clj/atomist/skill_meta.clj
+++ src/main/clj/atomist/skill_meta.clj
@@ -3,6 +3,6 @@
 
 (def module-name "gcf-java11-clojure")
 (def path ".")
-(def version "0.1.2")
-(def tag "0.1.2")
-(def generated-at "2020-06-05T04:31:33Z")
+(def version "0.1.3")
+(def tag "0.1.3")
+(def generated-at "2020-06-05T20:28:11Z")
